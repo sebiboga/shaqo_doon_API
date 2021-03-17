b1bd16a85ee4814e7564d53038db815c92fe4225
sebastianboga <sebastian.boga@endava.com>
2021-03-17 13:15:43 +0200

    creating version V2 for API

---
+++ v2/addjobs/index.php
@@ -0,0 +1,79 @@
+<?php 
+ /* prepare the id for each row in the database; id is a GUID */
+function guid(){
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = 
+		     substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12)
+            ;
+        return $uuid;
+}
+
+
+
+function city_translate($city) {
+    
+    
+    $result = $city;
+    if ($city=='Brasov')        {$result = 'Brașov';}
+    if ($city=='Iasi')          {$result = 'Iași';}
+    if ($city=='Pitesti')       {$result = 'Pitești';}
+    if ($city=='Chisinau')      {$result = 'Chișinău';}
+    if ($city=='Bucharest')     {$result = 'București';}
+    if ($city=='Targu Mures')   {$result = 'Târgu Mureș';}
+    if ($city=='Targu-Mures')   {$result = 'Târgu Mureș';}
+    if ($city=='Bucuresti')     {$result = 'București';}
+    if ($city=='Galati')        {$result = 'Galați';}
+    if ($city=='Timisoara')     {$result = 'Timișoara';}
+    if ($city=='Brasov')        {$result = 'Brașov';}
+    
+    
+    if ($city=='Balotesti')     {$result = 'Balotești';}
+    
+    
+    return $result;
+}
+ 
+ 
+ 
+ 
+ 
+ function country_translate($country) {
+    
+    $result = $country;
+    if ($country=='Romania') {$result = 'România';}
+    
+    return $result;
+}
+ 
+ 
+ 
+ $id = guid();
+ $obj = new stdClass();
+ $obj->id = $id;
+ $obj = new stdClass();
+ $obj->company = $_POST['company'];
+ $obj->title   = $_POST['title'];
+ $obj->city    = city_translate($_POST['city']);
+ $obj->country    = country_translate($_POST['country']);
+ $obj->link    = $_POST['link'];
+
+
+if ($_POST['action']=='insert')
+{
+
+require_once('../db/db_connect.php');
+
+
+ $sql = "INSERT INTO jobs (id,company,title,city,country,link) VALUES ('$id','$obj->company','$obj->title','$obj->city','$obj->country','$obj->link')";
+
+
+mysqli_query($concompany, $sql);
+mysqli_close($concompany);
+}
+?>
